c50d2de7ad518d0f8430875fbc1957d0364c8e45
Hiroki Yanaoka <40460282+yanaokahiroki@users.noreply.github.com>
2021-09-22 22:12:21 +0900

    :bug:  国際化対応のバグを修正

--- src/main/java/com/restful/api/exception/ProductAlreadyExistsException.java
+++ src/main/java/com/restful/api/exception/ProductAlreadyExistsException.java
@@ -1,16 +1,12 @@
 package com.restful.api.exception;
 
-import lombok.extern.slf4j.Slf4j;
-
 /**
  * リクエストに対して既にDBに同タイトルの商品が存在する場合の例外
  *
  * @author yanaokahiroki
  */
-@Slf4j
 public class ProductAlreadyExistsException extends RuntimeException{
   public ProductAlreadyExistsException(String message){
     super(message);
-    log.warn(message);
   }
 }
