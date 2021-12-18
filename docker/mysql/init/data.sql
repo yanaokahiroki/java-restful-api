9fce65c50d249d36b0b71a38527adef23e939dfb
Hiroki Yanaoka <40460282+yanaokahiroki@users.noreply.github.com>
2021-12-18 20:49:25 +0900

    :wrench:  Dockerのネットワーク設定を追加

--- docker/mysql/init/data.sql
+++
@@ -1,7 +0,0 @@
-# 初期データ投入
-insert into products (title, body, price)
-values ('test01', 'body01', 100),
-       ('test02', 'body02', 200),
-       ('test03', 'body03', 300),
-       ('test04', 'body04', 400),
-       ('test05', 'body05', 500);
